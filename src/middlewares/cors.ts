57cfb0961e677d54643bc3236679d535ae8f62a1
Ramon Nunes <ramontsdev@gmail.com>
2023-01-25 11:08:08 -0300

    cors

--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -11,7 +11,7 @@ export function cors(request: Request, response: Response, next: NextFunction) {
   const isAllowed = allowedOrigins.includes(origin);
 
   if (isAllowed) {
-    response.setHeader('Access-Control-Allow-Origin', origin);
+    response.setHeader('Access-Control-Allow-Origin', '*');
     response.setHeader('Access-Control-Allow-Methods', '*');
     response.setHeader('Access-Control-Allow-Headers', '*');
     response.setHeader('Access-Control-Allow-Max-Age', '10');
