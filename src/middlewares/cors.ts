28c2dfb69420e46899c9711f6bc21d4aff7f424b
Ramon Nunes <ramontsdev@gmail.com>
2023-01-25 11:09:59 -0300

    cors

--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -1,21 +1,26 @@
 import { NextFunction, Request, Response } from "express";
 
 export function cors(request: Request, response: Response, next: NextFunction) {
-  const allowedOrigins = [
-    'http://localhost:3000',
-    'http://localhost:3002',
-    'http://192.168.0.100:3000',
-  ];
+  /*   const allowedOrigins = [
+      'http://localhost:3000',
+      'http://localhost:3002',
+      'http://192.168.0.100:3000',
+    ];
 
-  const origin = request.header('origin')!;
-  const isAllowed = allowedOrigins.includes(origin);
+    const origin = request.header('origin')!;
+    const isAllowed = allowedOrigins.includes(origin);
 
-  if (isAllowed) {
-    response.setHeader('Access-Control-Allow-Origin', '*');
-    response.setHeader('Access-Control-Allow-Methods', '*');
-    response.setHeader('Access-Control-Allow-Headers', '*');
-    response.setHeader('Access-Control-Allow-Max-Age', '10');
-  }
+    if (isAllowed) {
+      response.setHeader('Access-Control-Allow-Origin', '*');
+      response.setHeader('Access-Control-Allow-Methods', '*');
+      response.setHeader('Access-Control-Allow-Headers', '*');
+      response.setHeader('Access-Control-Allow-Max-Age', '10');
+    } */
+
+  response.setHeader('Access-Control-Allow-Origin', '*');
+  response.setHeader('Access-Control-Allow-Methods', '*');
+  response.setHeader('Access-Control-Allow-Headers', '*');
+  response.setHeader('Access-Control-Allow-Max-Age', '10');
 
   next();
 };
